910b565c8ba81acfc21056a7600cdf74f487dcb2
RAJATsd <rajatsharmaatri@gmail.com>
2023-10-05 23:30:39 +0530

    fix: message addition order

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
   };
 
   const addMessage = (newMessage) => {
-    const newMessageList = [newMessage, ...messageList];
+    const newMessageList = [...messageList, newMessage];
     setMessageList(newMessageList);
   };
 
